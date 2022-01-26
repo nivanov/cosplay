df850f50a8ac074596ba8ec9ae26b5fc4a153b45
Vlad Ivanov <vlad94568@gmail.com>
2022-01-25 18:57:25 -0800

    Added help menus to pong.

--- modules/cosplay/src/main/scala/org/cosplay/examples/games/pong/CPPongTitleScene.scala
+++ modules/cosplay/src/main/scala/org/cosplay/examples/games/pong/CPPongTitleScene.scala
@@ -43,13 +43,49 @@ object CPPongTitleScene extends CPScene("title", None, bgPx):
             case ':' => px.withFg(C_GREY70)
             case _ => px
     ).trimBg()
+
+    private val helpImg4 = FIG_DOOM.renderMulti("[Q] - Quit", C_WHITE).trimBg()
+    private val helpImg3 = FIG_DOOM.renderMulti("[S] - Down", C_WHITE).trimBg()
+    private val helpImg2 = FIG_DOOM.renderMulti("[W] - Up", C_WHITE).trimBg()
+    private val helpImg1 = FIG_DOOM.renderMulti("[ENTER] - Start", C_WHITE).trimBg()
+
     private val logoW = logoImg.getWidth
     private val logoH = logoImg.getHeight
     private val logoSpr = new CPImageSprite("logoSpr", 0, 0, 0, logoImg, shaders = Seq(CPFadeInShader(true, 1500, bgPx))):
         override def update(ctx: CPSceneObjectContext): Unit =
             val canv = ctx.getCanvas
             setX((canv.dim.width - logoImg.getWidth) / 2)
-            setY((canv.dim.height - logoImg.getHeight) / 2)
+            setY((canv.dim.height - logoImg.getHeight) / 20)
+
+
+    private val helpSpr4 = new CPImageSprite("helpSpr4", 0, 0, 0, helpImg4, shaders = Seq(CPFadeInShader(true, 1500, bgPx))):
+        override def update(ctx: CPSceneObjectContext): Unit =
+            val canv = ctx.getCanvas
+            setX((canv.dim.width - helpImg4.getWidth) / 2)
+            setY((canv.dim.height - helpImg4.getHeight))
+
+    private val helpSpr3 = new CPImageSprite("helpSpr3", 0, 0, 0, helpImg3, shaders = Seq(CPFadeInShader(true, 1500, bgPx))):
+        override def update(ctx: CPSceneObjectContext): Unit =
+            val canv = ctx.getCanvas
+            setX((canv.dim.width - helpImg3.getWidth) / 2)
+            setY((canv.dim.height - helpImg3.getHeight) / 4)
+
+    private val helpSpr2 = new CPImageSprite("helpSpr2", 0, 0, 0, helpImg2, shaders = Seq(CPFadeInShader(true, 1500, bgPx))):
+        override def update(ctx: CPSceneObjectContext): Unit =
+            val canv = ctx.getCanvas
+            setX((canv.dim.width - helpImg2.getWidth) / 2)
+            setY((canv.dim.height - helpImg2.getHeight) / 8)
+
+    private val helpSpr1 = new CPImageSprite("helpSpr1", 0, 0, 0, helpImg1, shaders = Seq(CPFadeInShader(true, 1500, bgPx))):
+        override def update(ctx: CPSceneObjectContext): Unit =
+            val canv = ctx.getCanvas
+            setX((canv.dim.width - helpImg1.getWidth) / 2)
+            setY((canv.dim.height - helpImg1.getHeight) / 12)
+
 
     // Add scene objects...
     addObject(logoSpr)
+    addObject(helpSpr4)
+    addObject(helpSpr3)
+    addObject(helpSpr2)
+    addObject(helpSpr1)
