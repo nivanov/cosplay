f58bceef11998aa70040845374867bc11353b53d
Nikita Ivanov <nivanov@kernal.local>
2022-02-12 10:27:54 -0800

    WIP

--- modules/cosplay/src/main/scala/org/cosplay/impl/emuterm/CPEmuTerminal.scala
+++ modules/cosplay/src/main/scala/org/cosplay/impl/emuterm/CPEmuTerminal.scala
@@ -140,13 +140,14 @@ class CPEmuTerminal(gameInfo: CPGameInfo) extends CPTerminal:
     private def initFontMetrics(): Unit =
         termFont = new Font(fontName, Font.PLAIN, fontSize)
         // Correct for extra-wide monospace fonts on Mac OS and Linux.
-        if !CPUtils.isSysEnvSet("COSPLAY_EMUTERM_CH_WIDTH_OFFSET") && (SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_LINUX) then chWOff = -5
+//        if !CPUtils.isSysEnvSet("COSPLAY_EMUTERM_CH_WIDTH_OFFSET") && (SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_LINUX) then chWOff = -5
         val g = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB).createGraphics()
         configFont(g)
         val fm = g.getFontMetrics(termFont)
         descent = fm.getMaxDescent
         chW = fm.getWidths.max + chWOff
         chH = fm.getHeight - fm.getLeading + chHOff
+        println(s"maxWidth=${fm.getWidths.max}, height=${fm.getHeight}")
         g.dispose()
 
     /**
