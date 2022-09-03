7a938a891f619dbacf1f693a5111acd771a081ed
Nikita Ivanov <>
2022-09-02 17:36:46 -0700

    WIP

--- modules/cosplay/src/main/scala/org/cosplay/games/mir/os/progs/mash/compiler/CPMirMashCompilerResult.scala
+++ modules/cosplay/src/main/scala/org/cosplay/games/mir/os/progs/mash/compiler/CPMirMashCompilerResult.scala
@@ -25,48 +25,12 @@ package org.cosplay.games.mir.os.progs.mash.compiler
    \____/  \____//____/ /_/     /_/  \__,_/ _\__, /
                                             /____/
 
-          2D ASCII GAME ENGINE FOR SCALA3
-            (C) 2021 Rowan Games, Inc.
-               ALl rights reserved.
+          2D ASCII JVM GAME ENGINE FOR SCALA3
+              (C) 2021 Rowan Games, Inc.
+                ALl rights reserved.
 */
-
-import org.cosplay.games.mir.os.progs.mash.compiler.antlr4.*
-import org.antlr.v4.runtime.tree.*
-import org.antlr.v4.runtime.*
 
 /**
   *
   */
-class CPMirMashCompiler:
-    /**
-      *
-      * @param code
-      * @param origin
-      */
-    class FiniteStateMachine(code: String, origin: String) extends CPMirMashBaseListener
-
-    /**
-      *
-      * @param code
-      * @param origin
-      */
-    private def antlr4Setup(code: String, origin: String): (FiniteStateMachine, CPMirMashParser) =
-        val lexer = new CPMirMashLexer(CharStreams.fromString(code, origin))
-        val parser = new CPMirMashParser(new CommonTokenStream(lexer))
-
-        // State automata + it's parser.
-        new FiniteStateMachine(code, origin) -> parser
-
-    /**
-      *
-      * @param code
-      * @param origin
-      */
-    def compile(code: String, origin: String): CPMirMashCompilerResult =
-        val (fsm, parser) = antlr4Setup(code, origin)
-
-        // Parse the input IDL and walk built AST.
-        (new ParseTreeWalker).walk(fsm, parser.mash())
-
-        null // TODO
-
+class CPMirMashCompilerResult
