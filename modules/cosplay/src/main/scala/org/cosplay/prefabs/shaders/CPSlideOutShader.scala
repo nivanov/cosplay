1f812bab282e68bae4664df641c6efcb74e53f3c
Nikita Ivanov <mk61hacker@gmail.com>
2023-10-20 18:49:41 -0700

    Fixing shader for changing screen dim.

--- modules/cosplay/src/main/scala/org/cosplay/prefabs/shaders/CPSlideOutShader.scala
+++ modules/cosplay/src/main/scala/org/cosplay/prefabs/shaders/CPSlideOutShader.scala
@@ -20,6 +20,7 @@ package org.cosplay.prefabs.shaders
 import org.cosplay.*
 import org.cosplay.prefabs.shaders.CPSlideDirection.*
 import org.apache.commons.math3.analysis.function.*
+import scala.util.*
 
 /*
    _________            ______________
@@ -136,19 +137,21 @@ class CPSlideOutShader(
             val canv = ctx.getCanvas
             rect.loop((x, y) => 
                 if canv.isValid(x, y) then
-                    val zpx = canv.getZPixel(x, y)
-                    val px = zpx.px
-                    if px != bgPx && !skip(zpx, x, y) then
-                        val px = zpx.px
-                        val mx = x - rect.x
-                        val my = y - rect.y
-                        if mx >= 0 && mx < lastDim.w && my >= 0 && my < lastDim.h then
-                            val maxFrame = matrix(mx)(my)
-                            val bal = if frmCnt >= maxFrame then 1f else balance(frmCnt, maxFrame)
-                            !>(bal >= 0f && bal <= 1f, s"Invalid balance value: $bal (must be in [0,1] range).")
-                            val newFg = CPColor.mixture(px.fg, bgFg, bal)
-                            val newBg = px.bg.flatMap(CPColor.mixture(_, bgBg, bal).?)
-                            canv.drawPixel(px.withFgBg(newFg, newBg), x, y, zpx.z)
+                    Try(canv.getZPixel(x, y)) match
+                        case Success(zpx) =>
+                            val px = zpx.px
+                            if px != bgPx && !skip(zpx, x, y) then
+                                val px = zpx.px
+                                val mx = x - rect.x
+                                val my = y - rect.y
+                                if mx >= 0 && mx < lastDim.w && my >= 0 && my < lastDim.h then
+                                    val maxFrame = matrix(mx)(my)
+                                    val bal = if frmCnt >= maxFrame then 1f else balance(frmCnt, maxFrame)
+                                    !>(bal >= 0f && bal <= 1f, s"Invalid balance value: $bal (must be in [0,1] range).")
+                                    val newFg = CPColor.mixture(px.fg, bgFg, bal)
+                                    val newBg = px.bg.flatMap(CPColor.mixture(_, bgBg, bal).?)
+                                    canv.drawPixel(px.withFgBg(newFg, newBg), x, y, zpx.z)
+                        case _ => () // Ignore.
             )
             frmCnt += 1
             if frmCnt == maxFrmCnt then
